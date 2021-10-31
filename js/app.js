50e53d3425b6d02ebcfc5059a2f2861032ed5b73
JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>
2021-10-30 20:36:20 -0700

    changing sw path installing in app.js
    Signed-off-by: JOSE ALONSO LOPEZ ROMO <alonso.lopez.r@gmail.com>

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
 if (navigator.serviceWorker) {
     console.log("Si esta disponible el trabajo con el Service worker");
     //instyalar el service worker
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('../sw.js');
 } else
     console.log("NO esta disponible el trabajo con el Service worker en este browser");
